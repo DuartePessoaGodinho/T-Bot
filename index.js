ad1ba9c26aef35c3d7c2d72394b9f71112ef0aa7
Duarte <duarte.r.godinho@gmail.com>
2022-01-06 15:47:01 +0000

    duarte

--- index.js
+++ index.js
@@ -3,22 +3,23 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const client = new Client({ 
-    intents: [ 
-        Intents.FLAGS.GUILDS, 
+const client = new Client({
+    intents: [
+        Intents.FLAGS.GUILDS,
         Intents.FLAGS.GUILD_MESSAGES
-    ] 
-}); 
+    ]
+});
 
 client.on("ready", () => {
-  console.log('Logged in.')
+    console.log('Logged in.')
 })
 
-client.on('message', msg => {
-  if(msg.content === "ping") {
-    // msg.reply('pong');
-    msg.channel.send('new commit');
-  }
+client.on('messageCreate', msg => {
+    const message = msg.content.toLowerCase();
+    if (message.includes('duarte') || message.includes('dgodinho') || message.includes('dûart')) {
+        msg.reply('O que é que queres do boss ?');
+        // msg.channel.send('new commit');
+    }
 })
 
 
